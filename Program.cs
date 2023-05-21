d4d25d2a5703ce5be59b6e6ba55e2f345b6e4c14
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-05-21 11:54:06 -0300

    Exercício Corrigido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("=-= Mes por Extenso =-=");
+
+Console.Write("Digite um número de 1 a 12: ");
+int numeroMes = int.Parse(Console.ReadLine()!);
+
+string[] meses = {
+    "Janeiro", "Fevereiro", "Março", "Abril",
+    "Maio", "Junho", "Julho", "Agosto",
+    "Setembro", "Outubro", "Novembro", "Dezembro"
+};
+
+string nomeMes = (numeroMes >= 1 && numeroMes <= 12) ? meses[numeroMes - 1] : "Mês inválido";
+
+Console.WriteLine("O número {0} corresponde ao mês de {1}.", numeroMes, nomeMes);
+
